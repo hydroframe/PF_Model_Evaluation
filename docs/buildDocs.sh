dc9c769c07b28efd13cc8738ea79770f54a3c687
Vineet Bansal <vineetbansal@protonmail.com>
2020-10-01 11:08:23 -0400

    simplified build_docs steps
    Since we're building the apidocs, it's safer to install the package with dev dependencies and keep the buildDocs.sh script minimal.

--- docs/buildDocs.sh
+++ docs/buildDocs.sh
@@ -1,41 +1,12 @@
 #!/bin/bash
+
+# Adapted from:
+# https://tech.michaelaltfield.net/2020/07/18/sphinx-rtd-github-pages-1/
+
 set -x
-################################################################################
-# File:    buildDocs.sh
-# Purpose: Script that builds our documentation using sphinx and updates GitHub
-#          Pages. This script is executed by:
-#            .github/workflows/docs_pages_workflow.yml
-#
-# Authors: Michael Altfield <michael@michaelaltfield.net>
-# Created: 2020-07-17
-# Updated: 2020-07-17
-# Version: 0.1
-################################################################################
-
-###################
-# INSTALL DEPENDS #
-###################
-
-apt-get update
-apt-get -y install git rsync python3-sphinx python3-sphinx-rtd-theme
-
-#####################
-# DECLARE VARIABLES #
-#####################
-
 pwd
 ls -lah
 export SOURCE_DATE_EPOCH=$(git log -1 --pretty=%ct)
-
-##############
-# BUILD DOCS #
-##############
-
-# build our documentation with sphinx (see docs/conf.py)
-# * https://www.sphinx-doc.org/en/master/usage/quickstart.html#running-the-build
-(cd docs && sphinx-apidoc -f -o ./source ../src -H Modules)
-make -C docs clean
-make -C docs html
 
 #######################
 # Update GitHub Pages #
@@ -54,23 +25,14 @@ git init
 git remote add deploy "https://token:${GITHUB_TOKEN}@github.com/${GITHUB_REPOSITORY}.git"
 git checkout -b gh-pages
 
-# add .nojekyll to the root so that github won't 404 on content added to dirs
-# that start with an underscore (_), such as our "_content" dir..
-touch .nojekyll
-
 # Add README
 cat > README.md <<EOF
 # GitHub Pages Cache
 
-Nothing to see here. The contents of this branch are essentially a cache that's not intended to be viewed on github.com.
+Nothing to see here.
 
 The actual documentation is available at: #coming-soon
 
-If you're looking to update our documentation, check the relevant development branch's 'docs/' dir.
-
-For more information on how this documentation is built using Sphinx, Read the Docs, and GitHub Actions/Pages, see:
-
- * https://tech.michaelaltfield.net/2020/07/18/sphinx-rtd-github-pages-1
 EOF
 
 # copy the resulting html pages built from sphinx above to our new git repo
