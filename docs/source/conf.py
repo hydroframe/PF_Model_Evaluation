f13ce647462987ad4ec03a2bb76a450b3fc7dc99
Vineet Bansal <vineetbansal@protonmail.com>
2020-10-01 10:30:58 -0400

    sphinxcontrib/numpydoc dependencies removed for gh pages.

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -32,7 +32,7 @@ import pfspinup
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ['sphinx.ext.autodoc', 'sphinx.ext.mathjax', 'sphinxcontrib.bibtex', 'numpydoc']
+extensions = ['sphinx.ext.autodoc', 'sphinx.ext.mathjax']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
