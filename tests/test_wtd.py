20af71e9a7565062102f81de90c99d0616c3e612
Vineet Bansal <vineetbansal@protonmail.com>
2021-03-22 15:38:56 -0400

    added a new failing new test in preparation for making wtd/overland flow etc. accessible through Data Accessor.

--- tests/test_wtd.py
+++ tests/test_wtd.py
@@ -1,3 +1,4 @@
+import pytest
 import numpy as np
 import pfspinup.pfio as pfio
 from pfspinup.common import calculate_water_table_depth
@@ -22,5 +23,20 @@ def test_water_table_depth(metadata, test_data_dir):
         saturation[mask == 0] = np.nan
 
         wtd[i, ...] = calculate_water_table_depth(pressure, saturation, dz)
+
+    assert np.allclose(wtd, np.load(f'{test_data_dir}/wtd.npy'), equal_nan=True)
+
+
+@pytest.mark.xfail
+def test_water_table_depth_data_accessor(run, test_data_dir):
+    data = run.data_accessor
+    nt = len(data.times)
+    ny = run.ComputationalGrid.NY
+    nx = run.ComputationalGrid.NX
+
+    wtd = np.zeros((nt, ny, nx))
+    for i in data.times:
+        wtd[i, ...] = data.wtd  # TODO: Needs to be implemented
+        data.time += 1
 
     assert np.allclose(wtd, np.load(f'{test_data_dir}/wtd.npy'), equal_nan=True)
